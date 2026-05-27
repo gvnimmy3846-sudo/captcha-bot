89652df05ce0f003f607a59efa2b89f3d1ffceae
gvnimmy3846-sudo <gvnimmy3846@gmail.com>
2026-05-27 11:28:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler
 
 TOKEN = "8838766761:AAFQRp1bgIiUjCgIaywmDMD_hmRfUO49op8"
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot Working ✅")
+async def start(update, context):
+    await update.message.reply_text("Bot working!")
 
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
-
-print("Bot Started ✅")
 
 app.run_polling()
