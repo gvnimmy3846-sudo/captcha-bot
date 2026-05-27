10372655b4346fbe1afe36c145104fc221a79020
gvnimmy3846-sudo <gvnimmy3846@gmail.com>
2026-05-27 11:10:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 TOKEN = "8838766761:AAFQRp1bgIiUjCgIaywmDMD_hmRfUO49op8"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot Working Successfully ✅")
+    await update.message.reply_text("Bot Working ✅")
 
 app = ApplicationBuilder().token(TOKEN).build()
 
